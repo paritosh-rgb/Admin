8f1635359cd0c140387d5d97d271c70d57f023a3
paritosh-rgb <paritosh505@gmail.com>
2020-07-22 18:37:54 +0530

    admin

---
+++ app/src/main/java/com/example/adminhomepage/Updatescheme.java
@@ -0,0 +1,14 @@
+package com.example.adminhomepage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Updatescheme extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_updatescheme);
+    }
+}
---
+++ app/src/main/java/com/example/adminhomepage/Uploadnewscheme.java
@@ -0,0 +1,14 @@
+package com.example.adminhomepage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Uploadnewscheme extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uploadnewscheme);
+    }
+}
